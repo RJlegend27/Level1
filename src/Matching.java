bb4ff8b33644b9ad5bac36059a1c1c479ee62f22
gerings17 <skyswims@yahoo.com>
2017-02-15 18:30:25 -0800

    matching

---
+++ src/Matching.java
@@ -0,0 +1,43 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Matching {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+
+		JButton star = new JButton();
+		JButton square = new JButton();
+		JButton no = new JButton();
+		JButton yes = new JButton();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		star.setText("star");
+		panel.add(star);
+		panel.add(square);
+		square.setText("square");
+		panel.add(no);
+		no.equals(square);
+		panel.add(yes);
+		yes.equals(star);
+		star.addActionListener(this);
+		square.addActionListener(this);
+		no.addActionListener(this);
+		yes.addActionListener(this);
+		if(e.getSource() instanceof JButton){
+			if(counter == 1%)
+					text1 = ((JButton)e.getSource())getText());
+			e.getSource.show(true);
+			)
+else if 1 // conter even)
+text2 = show(true);
+//check
+popup = it wrong()
+		}
+
+	}
+}
